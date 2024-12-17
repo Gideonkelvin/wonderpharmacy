0370466c2b1722d0f0d2b82679834154a479b123
Joseph <indiekaj@gmail.com>
2024-12-17 11:41:36 +0300

    created contact us model and forms

--- core/models.py
+++ core/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
+class ContactUs(models.Model):
+    name = models.CharField(max_length=25)
+    email = models.EmailField()
+    subject = models.CharField(max_length=30)
+    message = models.TextField()
+    
+    class Meta:
+        verbose_name_plural = "Contact Us"
+    
+    def __str__(self):
+        return f"{self.name} with the subject {self.subject}"
+
 # Create your models here.
